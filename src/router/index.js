3ff4ba4ad5749b9b0f3fbe3f0735e5a9eacfd5fd
Sejeong Kim <clearrworld@gmail.com>
2024-08-02 14:48:03 +0900

    chore: router 설정

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeComponent from '@/components/HomeComponent.vue'
+
+const routes = [
+    {
+        path: '/home',
+        name: 'HOME',
+        component: HomeComponent
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router

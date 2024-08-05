a47848af30504bba1dfc9580ff006cadfc3a5147
sseho <kindseho@naver.com>
2024-08-05 21:05:14 +0900

    feat: 버튼 컴포넌트 구현

---
+++ src/components/test/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-btn class="rounded-xl text-body-1 font-weight-bold" :style="{ color: '#094F08', background: '#A4DEC6' }">
+        {{ content }}
+    </v-btn>
+    <v-btn class="rounded-xl text-body-1 font-weight-bold" :style="{ color: 'white', background: '#094F08' }">
+        {{ content }}
+    </v-btn>
+    <v-btn class="rounded-xl text-h6 font-weight-bold" variant="flat" :style="{ color: '#094F08' }">{{
+        content }}</v-btn>
+</template>
+
+<script>
+export default {
+    name: 'ButtonComponent',
+    props: ['content'],
+    data() {
+        return {
+
+        }
+    },
+    watch: {
+
+    },
+    updated() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+}
+</script>
--- src/components/test/HomeComponent.vue
+++ src/components/test/HomeComponent.vue
@@ -5,20 +5,33 @@
     </v-row>
     <v-row>
       <v-col class="d-flex justify-start" cols="1">
-        <v-btn color="sid_green">시그니처 색</v-btn>
+        <v-btn outlined color="sid_green">시그니처 색</v-btn>
       </v-col>
       <v-col class="d-flex justify-start" cols="1">
-        <v-btn color="sid_btn1">버튼 색1</v-btn>
+        <v-btn outlined color="sid_btn1">버튼 색1</v-btn>
       </v-col>
       <v-col class="d-flex justify-start" cols="1">
-        <v-btn color="sid_btn2">버튼 색2</v-btn>
+        <v-btn outlined color="sid_btn2">버튼 색2</v-btn>
       </v-col>
-    </v-row>
+    </v-row><br>
+    <ButtonComponent content="Si-D" /><br><br>
+    <ButtonComponent2 content="Si-D" color="#A4DEC6" background="#094F08"/>
   </v-container>
 </template>
 
 <script>
+import ButtonComponent from './ButtonComponent.vue';
+import ButtonComponent2 from './ButtonComponent2.vue';
+
 export default {
+  components: {
+    ButtonComponent,ButtonComponent2
+    },
+    methods:{
+      test(){
+        alert("test")
+      }
+    }
 }
 </script>
 

57e9e493ec07ad200a91cf345d2716fccc4c0e9f
wisdom0405 <wisdom0405@kakao.com>
2024-08-07 20:02:45 +0900

    merge 충돌해결

--- src/router/LaunchedProjectRouter.js
+++ src/router/LaunchedProjectRouter.js
@@ -1,6 +1,10 @@
 import LaunchedProjectList from '@/views/LaunchedProjectList.vue'
 
+<<<<<<< Updated upstream
 export const LaunchedProjectRouter = [
+=======
+const LaunchedProjectRouter = [
+>>>>>>> Stashed changes
     {
         path:'/launched-project/list',
         name: 'LaunchedProjectList',

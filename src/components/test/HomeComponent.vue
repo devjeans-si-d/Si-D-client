0319da5c50bd74f785a871ff0ef80df4a3ed1a60
getsetgo1 <dnwls1313@gmail.com>
2024-08-08 04:15:48 +0900

    modal 검색 api 적용

--- src/components/test/HomeComponent.vue
+++ src/components/test/HomeComponent.vue
@@ -19,61 +19,137 @@
     </v-row>
     <v-row>
       <v-col>사람칩</v-col>
-      <v-col><AvatarChip url="https://sejeong-file.s3.ap-northeast-2.amazonaws.com/sid-practice/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2024-08-05+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+10.08.49.png" name="최세호"/></v-col>
+      <v-col
+        ><AvatarChip
+          url="https://sejeong-file.s3.ap-northeast-2.amazonaws.com/sid-practice/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2024-08-05+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+10.08.49.png"
+          name="최세호"
+      /></v-col>
     </v-row>
-    
+
     <v-row>
       <v-col>기술 스택 칩(closeable)</v-col>
       <v-col class="d-flex justify-start" cols="1.5">
-        <CloseableChip title="Spring" color="be_blue"/>
+        <CloseableChip title="Spring" color="be_blue" />
       </v-col>
       <v-col class="d-flex justify-start" cols="1.5">
-        <CloseableChip title="React" color="fe_yellow"/>
+        <CloseableChip title="React" color="fe_yellow" />
       </v-col>
       <v-col>기술 스택 칩(basic)</v-col>
       <v-col class="d-flex justify-start" cols="1.5">
-        <BasicChip title="Figma" color="de_purple"/>
+        <BasicChip title="Figma" color="de_purple" />
       </v-col>
       <v-col class="d-flex justify-start" cols="1.5">
-        <BasicChip title="Swift" color="app_red"/>
+        <BasicChip title="Swift" color="app_red" />
       </v-col>
       <v-col class="d-flex justify-start" cols="1.5 ">
-        <BasicChip title="JIRA" color="pm_green"/>
+        <BasicChip title="JIRA" color="pm_green" />
       </v-col>
-    </v-row> 
-
+    </v-row>
 
     <!-- Footer입니다 -->
     <v-row>
-      <FooterComponent/>
+      <FooterComponent />
     </v-row>
   </v-container>
 
+  <!-- 인원 검색 modal -->
+  <v-btn @click.stop="searchMemberShowModal()">멤버 검색</v-btn>
+  <!-- 모달창을 위한 v-dialog -->
+  <v-dialog v-model="dialog" max-width="600px">
+    <v-card>
+      <v-form @submit.prevent="searchMembers">
+        <v-row>
+          <v-col cols="auto">
+            <v-select
+              v-model="searchType"
+              :items="searchOptions"
+              item-title="text"
+              item-value="value"
+            >
+            </v-select>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="searchValue" label="Search"> </v-text-field>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn type="submit">검색</v-btn>
+          </v-col>
+          <v-data-table :headers="tableHeaders" :items="memberList">
+          </v-data-table>
+        </v-row>
+      </v-form>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-import CloseableChip from '@/components/chip/CloseableChip.vue';
-import BasicChip from '@/components/chip/BasicChip.vue';
-import FooterComponent from '@/components/footer/FooterComponent.vue';
-import AvatarChip from '@/components/chip/AvatarChip.vue';
-import ButtonComponent from '../button/ButtonComponent.vue';
+import CloseableChip from "@/components/chip/CloseableChip.vue";
+import BasicChip from "@/components/chip/BasicChip.vue";
+import FooterComponent from "@/components/footer/FooterComponent.vue";
+import AvatarChip from "@/components/chip/AvatarChip.vue";
+import ButtonComponent from "../button/ButtonComponent.vue";
+import axios from "axios";
 
 export default {
-  data(){
-
+  data() {
+    return {
+      tableHeaders: [
+        { title: "이름", key: "name", align: "center" },
+        { title: "EMAIL", key: "email", align: "center" },
+        { title: "닉네임", key: "nickname", align: "center" },
+      ],
+      dialog: false,
+      searchType: "optional",
+      searchOptions: [
+        { text: "이름", value: "name" },
+        { text: "이메일 아이디", value: "email" },
+        { text: "닉네임", value: "nickname" },
+      ],
+      searchValue: "",
+      memberList: [],
+      pageSize: 5,
+      currentPage: 0,
+      isLastPage: false,
+    };
   },
+
   components: {
     CloseableChip,
     BasicChip,
     FooterComponent,
     AvatarChip,
-    ButtonComponent
+    ButtonComponent,
   },
-  methods:{
-      test(){
-        alert("test")
+  methods: {
+    test() {
+      alert("test");
+    },
+    searchMemberShowModal() {
+      this.dialog = true;
+    },
+    async searchMembers() {
+      if(this.isLastPage) return; 
+      this.memberList = [];
+      let params = {
+        // size: this.pageSize,
+        // page: this.currentPage,
+      };
+      if (this.searchType == "name") {
+        params.name = this.searchValue;
+      } else if (this.searchType == "email") {
+        params.email = this.searchValue;
       }
-    }
-}
-</script>
+      if (this.searchType == "nickname") {
+        params.nickname = this.searchValue;
+      }
 
+      const response = await axios.get(
+        `${process.env.VUE_APP_API_BASE_URL}/api/member/list`,
+        { params }
+      );
+      this.isLastPage = response?.data?.result?.last;
+      this.memberList=response?.data?.content;
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,52 @@ import App from './App.vue'
 import router from '@/router/index.js'
 import vuetify from './plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
-
+import axios from 'axios'
 const app = createApp(App);
-
+axios.interceptors.request.use(
+    (config) => {
+    //   const token = localStorage.getItem("token");
+  
+    //   if (token) {
+    //     config.headers["Authorization"] = `Bearer ${token}`;
+    //   }
+      config.headers["Authorization"] = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MjI0MjYxMjYsImV4cCI6MTcyNTAxODEyNn0.PrHtUt1jhj_zl2FNJGRirdnJQTsorAMXAzcgVHfybJg`;
+      
+  
+      return config;
+    },
+    (error) => {
+      // 해당 인터셉터가 무시되고, 사용자의 본래 요청인 화면으로 라우팅된다. (가던 길 가는 것)
+      return Promise.reject(error);
+    }
+  );
+  //401 응답을 받을 경우에 intercepter를 통해 전역적으로 rt를 통한 at 재발급
+  //만약 rt도 401 응답을 받을 경우에 -> 로그아웃 시켜줘야함(localstorage에서 토큰 제거 후 login 화면으로 리다이렉트)
+  axios.interceptors.response.use(
+    (response) => response,
+  
+    async (error) => {
+      console.log(error);
+      if (error.response && error.response.status === 401) {
+        const refreshToken = localStorage.getItem("refreshToken");
+        try {
+          localStorage.removeItem("token");
+          const response = await axios.post(
+            `${process.env.VUE_APP_API_BASE_URL}/refresh-token`,
+            { refreshToken }
+          );
+          localStorage.setItem("token", response.data.result.token);
+          // 토큰이 만료된 걸 다시 받아온 거니 화면 리로드해줘서 사용하면서 api에 새로운 리프레시토큰을 담겨서 요청하게 만들어야한다.
+          window.location.reload();
+          //   if(error.response.status === 403) throw error;
+        } catch (e) {
+          localStorage.clear();
+          window.location.href = "/login";
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
 app.use(router);
 app.use(vuetify);
 app.mount('#app');

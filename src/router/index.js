23dea6b7beeec4e14c8a5a497a53037158d86071
wisdom0405 <wisdom0405@kakao.com>
2024-08-07 14:19:27 +0900

    chore : LaunchedProject router 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeComponent from '@/components/test/HomeComponent.vue'
-
+import { LaunchedProjectRouter } from './LaunchedProjectRouter';
 const routes = [
     {
         path: '/home',
@@ -11,7 +11,8 @@ const routes = [
         path: '/',
         name: 'ROOT',
         component: HomeComponent
-    }
+    },
+    ...LaunchedProjectRouter
 ]
 
 const router = createRouter({

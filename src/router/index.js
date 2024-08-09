4679c77b41f6b4354e14f5113486d5b36c2528e5
sseho <kindseho@naver.com>
2024-08-09 19:26:21 +0900

    feat: 로그인페이지 생성

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeComponent from '@/components/test/HomeComponent.vue'
 import { siderCardRouter } from './siderCardRouter';
 import { LaunchedProjectRouter } from './LaunchedProjectRouter';
 import { memberRouter } from './memberRouter';
+import LoginPage from '@/views/LoginPage.vue';
 
 const routes = [
     {
@@ -15,6 +16,12 @@ const routes = [
         name: 'ROOT',
         component: HomeComponent
     },
+    {
+        path: '/login',
+        name: 'LoginPage',
+        component: LoginPage
+    },
+    
     
     ...siderCardRouter,
     ...LaunchedProjectRouter,
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>LoginPage</div>
+    <button @click="login"><img src="../assets/kakao_login_button.png" alt="카카오로그인"></button><br>
+    <a href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=f1a9f25e347069f2e5fedb6375c0b82d&redirect_uri=http://localhost:8080/api/auth/kakao/callback"><img src="../assets/kakao_login_button.png" alt="카카오로그인"></a>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                
+            }
+        },
+        created(){
+
+        },
+        watch:{
+            
+        },
+        updated() {
+
+        },
+        computed: {
+            
+        },
+        methods:{
+            login(){
+                window.location.href = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=f1a9f25e347069f2e5fedb6375c0b82d&redirect_uri=http://localhost:8080/api/auth/kakao/callback"
+                const code = new URL(document.location.toString()).searchParams.get('code');
+                console.log("code: ",code);
+                
+            }
+        }
+    }
+</script>

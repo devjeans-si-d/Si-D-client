6525235c0244e83beae47948a135d45dde27161f
Sejeong Kim <clearrworld@gmail.com>
2024-08-11 20:16:55 +0900

    feat: sockJS 연결(이슈 해결전

--- src/views/member/ChatroomView.vue
+++ src/views/member/ChatroomView.vue
@@ -17,10 +17,10 @@
 
         <v-row>
             <v-col>
-                <v-text-field></v-text-field>
+                <v-text-field v-model="chatMessage"></v-text-field>
             </v-col>
             <v-col>
-                <ButtonComponent content="전송"/>
+                <ButtonComponent @click="sendMessage()" content="전송"/>
             </v-col>
         </v-row>
 
@@ -31,6 +31,9 @@ import ButtonComponent from '@/components/button/ButtonComponent.vue';
 import axios from 'axios'
 import { useRoute } from 'vue-router';
 
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
+
 
 
 export default {
@@ -40,16 +43,69 @@ export default {
     data() {
         return {
             chatList: [],
-            chatroomId: 0
+            chatroomId: 0,
+            chatMessage: "",
+            stompClient: ""
         }
     },
     async created() {
         const route = useRoute();
         this.chatroomId = route.params.chatroomId;
-        
-        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/chat/chatroom/${this.chatroomId}/receiver/4`);
+        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/chat/chatroom/${this.chatroomId}`);
         console.log(response.data.content);
         this.chatList = response.data.content;
+        
+        
+
+        //== 연결하는 부분 ==//
+        // 방에 입장
+        this.connect()
+    },
+    methods: {
+        async sendMessage() {
+        
+            if (this.chatMessage && this.stompClient) {
+
+                const chatObj = {
+                    chatroomId: this.chatroomId,
+                    content: this.chatMessage,
+                    token: localStorage.getItem('token')
+                }
+
+;
+                this.stompClient.send("/pub/" + this.chatroomId, JSON.stringify(chatObj));
+                
+                this.chatMessage = "";
+            }
+        },
+        connect() {
+            const socket = new SockJS(`${process.env.VUE_APP_API_BASE_URL}/chat`);
+            this.stompClient = Stomp.over(socket);
+
+            // enter chatroom api - Concurrent Hashmap에 넣어주는 부분
+            axios.post(`${process.env.VUE_APP_API_BASE_URL}/api/chat/chatroom/${this.chatroomId}/enter`);
+
+            
+            // 헤더에 토큰 끼워넣는 부분
+            const authToken = localStorage.getItem('token');
+            this.stompClient.connect({Authorization: `Bearer ${authToken}`}, () => {
+                // 수신할 메시지를 구독합니다.
+                this.stompClient.subscribe('/sub/chatroom/' + this.chatroomId, async function (response) {
+                    console.log("line 100!!");
+                    console.log(response);
+                    this.addMessage(response);
+                });
+            });
+
+
+            socket.onclose = function() {
+                console.log('WebSocket connection closed for user');
+            }
+        },
+        addMessage(message) {
+            this.chatList.append(message);
+        }
+
     }
 }   
 

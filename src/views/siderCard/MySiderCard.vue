8d9051a3b8e93fafaa6711eacc0ad028367dd13d
sseho <kindseho@naver.com>
2024-08-13 21:21:27 +0900

    feat: 사이더카드 수정화면

--- src/views/siderCard/MySiderCard.vue
+++ src/views/siderCard/MySiderCard.vue
@@ -27,13 +27,33 @@
                     <h2>자기소개</h2>
                 </v-row>
                 <v-row class="textarea">
-                        <v-textarea
-                          :model-value="data.introduction"
-                          :rules="rules"
-                          label="자기소개"
-                          counter
-                          variant="outlined"
-                        ></v-textarea>
+                    <v-textarea :model-value="data.introduction" :rules="rules" label="자기소개" counter
+                        variant="outlined"></v-textarea>
+                </v-row>
+            </v-col>
+        </v-row>
+
+        <v-row class="line">
+            <v-col>
+                <v-row class="email">
+                    <v-text-field v-model="data.socialLink.email" prepend-icon="mdi-email" label="개인 이메일 주소" variant="solo-filled"
+                        max-width="1200"></v-text-field>
+                </v-row>
+                <v-row class="email">
+                    <v-text-field v-model="data.socialLink.github" prepend-icon="mdi-github" label="깃허브 주소" variant="solo-filled"
+                        max-width="1200"></v-text-field>
+                </v-row>
+                <v-row class="email">
+                    <v-text-field v-model="data.socialLink.behance" prepend-icon="mdi-beta" label="비핸스 주소" variant="solo-filled"
+                        max-width="1200"></v-text-field>
+                </v-row>
+                <v-row class="email">
+                    <v-text-field v-model="data.socialLink.linkedin" prepend-icon="mdi-linkedin" label="링크드인 주소" variant="solo-filled"
+                        max-width="1200"></v-text-field>
+                </v-row>
+                <v-row class="email">
+                    <v-text-field v-model="data.socialLink.etc" prepend-icon="mdi-home" label="개인 블로그 주소" variant="solo-filled"
+                        max-width="1200"></v-text-field>
                 </v-row>
             </v-col>
         </v-row>
@@ -70,31 +90,23 @@
         <v-row>
             <v-col>
                 <div class="d-flex justify-center">
-                    <v-date-input
-                      v-model="date"
-                      label="Select range"
-                      max-width="468"
-                      multiple="range"
-                    ></v-date-input>
-                  </div>
+                    <v-date-input v-model="date" label="Select range" max-width="1200" multiple="range"></v-date-input>
+                </div>
             </v-col>
         </v-row>
-        <FooterComponent />
     </v-container>
-
+    <div class="foot-margin"></div>
 </template>
 
 <script>
-import FooterComponent from '@/components/footer/FooterComponent.vue';
 import { VDateInput } from 'vuetify/labs/VDateInput'
 export default {
     components: {
-        FooterComponent,VDateInput
+        VDateInput
     },
     data() {
         return {
             jobFields: [
-                { name: '없음', selected: true },
                 { name: '프론트엔드', selected: false },
                 { name: '백엔드', selected: false },
                 { name: '안드로이드', selected: false },
@@ -111,11 +123,11 @@ export default {
                 jobField: "",
                 introduction: "",
                 socialLink: {
-                    email: "seho@naver.com",
-                    github: "sseho",
-                    behance: "behance",
-                    linkedin: "linkedin",
-                    etc: "etc"
+                    email: "",
+                    github: "",
+                    behance: "",
+                    linkedin: "",
+                    etc: ""
                 },
                 careers: [],
                 teckStacks: [],
@@ -158,6 +170,15 @@ export default {
 </script>
 
 <style scoped>
+.email {
+    min-height: 50px;
+    margin: 20px;
+}
+
+.foot-margin {
+    margin-bottom: 100px;
+}
+
 .mx-auto {
     margin-left: auto;
     margin-right: auto;
@@ -166,6 +187,7 @@ export default {
 
 .line {
     border-bottom: 1px solid;
+    padding-bottom: 20px;
 }
 
 .textarea {

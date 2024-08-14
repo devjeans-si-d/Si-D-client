f64e213bb96b860d89ab7fb145732a0fb5ab23da
getsetgo1 <dnwls1313@gmail.com>
2024-08-14 16:16:29 +0900

    project 작성화면 기능 구현 끝

--- src/router/projectRouter.js
+++ src/router/projectRouter.js
@@ -1,29 +1,16 @@
 
-import MemberAddComponent from "@/components/modal/MemberAddComponent.vue"
-import MemberSearchDialogueTest from "@/components/modal/MemberSearchDialogueTest.vue"
 import editorComponent from "@/components/editor/editorComponent.vue"
-// import ProjectCreate from "@/views/project/ProjectCreate.vue"
-import P3rojectCreate from "@/views/project/P3rojectCreate.vue"
+import projectCreate from "@/views/project/projectCreate.vue"
 export const projectRouter = [
-    {
-        path: '/MemberAddComponent',
-        name: 'MemberAddComponent',
-        component: MemberAddComponent
-    },
-    {
-        path: '/MemberSearchDialogueTest',
-        name: 'MemberSearchDialogueTest',
-        component: MemberSearchDialogueTest
-    },
     {
         path: '/editorComponent',
         name: 'editorComponent',
         component: editorComponent
     },
     {
-        path: '/ProjectCreate',
-        name: 'P3rojectCreate',
-        component: P3rojectCreate
+        path: '/project-create',
+        name: 'projectCreate',
+        component: projectCreate
     },
 
 ]

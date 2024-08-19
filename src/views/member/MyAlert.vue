cd821708a45d89662804f1371934a57e8b39e600
Sejeong Kim <clearrworld@gmail.com>
2024-08-20 00:34:53 +0900

    feat: 알림 페이지 구현

--- src/views/member/MyAlert.vue
+++ src/views/member/MyAlert.vue
@@ -1,11 +1,12 @@
 <template>
+<v-card>
     <h2 style="text-align:center; margin: 20px;">🔔 내 알림</h2>
     <div style="margin: auto; text-align:center; border-bottom: 1px solid #D4D4D4; width: 70%;">
     <br>
     </div>
 <v-container style="width: 50%;">
         <v-alert
-        v-if="!this.exist()"
+        v-if="this.aList.length == 0"
         class="d-flex justify-center"
         title="아직은 새로운 알림이 없네요!"
         type="success"
@@ -25,18 +26,17 @@
 
         <div v-for="al in this.aList" :key="al.id">
         <v-alert
-            style="margin-top: 10px;"
-            color="#2A3B4D"
-            density="compact"
-            icon="mdi-firework"
-            type="info"
-            variant="tonal"
-            class="d-flex justify-center"
-            v-if="al.isRead == 'N'"
+        border="top"
+        type="warning"
+        variant="outlined"
+        class="d-flex justify-center"
+        v-if="al.isRead == 'N'"
         >
         <p> {{ al.title }} </p>
         <p> {{ al.content }} </p>
-    </v-alert>
+        <p> {{ al.createdAt }} </p>
+        </v-alert>
+
 
         <v-alert
         style="margin-top: 10px;"
@@ -48,18 +48,13 @@
         class="d-flex justify-center"
         v-if="al.isRead == 'Y'"
         >
-        <p> {{ al.title }} </p>
+        <p style="font-weight: bold;"> {{ al.title }} </p>
         <p> {{ al.content }} </p>
+        <p style="color: gray; font-size: small"> {{ getDay(al.createdAt)}} {{ getTime(al.createdAt) }} </p>
     </v-alert>
         </div>
-
-
-
-
-
-
-
 </v-container>
+</v-card>
 </template>
 <script>
 import { mapGetters } from 'vuex';
@@ -90,13 +85,38 @@ export default {
         // 안읽은 알림 가져오기
         const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/api/alert`);
         this.aList = response.data;
-        console.log(this.aList);
     },
     methods: {
         exist() {
             const total = this.localChatCnt + this.localAlertCnt;
             if(total >= 1) return true;
             return false;
+        },
+        getTime(createdAt) {
+            const createdTime = new Date(createdAt);
+            let hour = createdTime.getHours();
+            let minute = createdTime.getMinutes();
+            let ampm;
+            if(hour < 12) {
+                ampm = '오전'
+            } else {
+                ampm = '오후'
+                hour -= 12;
+            }
+            if(hour < 10) {
+                hour = '0'+hour;
+            }
+
+            if(minute < 10) {
+                minute = '0'+minute;
+            }
+
+            return ampm + ' ' + hour + ':' + minute;
+        },
+        getDay(createdAt) {
+            const createdTime = new Date(createdAt);
+
+            return `${createdTime.getFullYear()}년 ${createdTime.getMonth()}월 ${createdTime.getDate()}일`; 
         }
     }
 

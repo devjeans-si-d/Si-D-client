96c78b4ea03527b6f948a0fc8b3bb95edba6dd21
getsetgo1 <dnwls1313@gmail.com>
2024-09-04 15:07:41 +0900

    vuetify css 시도

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js'
+import 'vuetify/styles';
 import vuetify from './plugins/vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 import axios from 'axios'
